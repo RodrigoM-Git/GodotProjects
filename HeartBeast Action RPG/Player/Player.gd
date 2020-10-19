d72f68e285dee3e1b8ad1807a406a4642bc376e7
Rodrigo Miguel Rojas s3784466 <s3784466@student.rmit.edu.au>
2020-10-20 04:22:55 +1100

    Completed episode 2

--- HeartBeast Action RPG/Player/Player.gd
+++ HeartBeast Action RPG/Player/Player.gd
@@ -1,15 +1,32 @@
 extends KinematicBody2D
 
+# constant variables
+const ACCELERATION = 50
+const MAX_SPEED = 200
+const FRICTION = 400
+
+# variables
 var velocity = Vector2.ZERO
 
+# runs every frame
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	
+	# player movement code
+	var input_vector = Vector2.ZERO
+	
+	# checks for direction
+	input_vector.x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
+	input_vector.y = Input.get_action_strength('ui_down') - Input.get_action_strength('ui_up')
+	
+	# normalizes input to normal (prevents double speed when two directions are pressed)
+	input_vector = input_vector.normalized()
+	
+	# sets direction and speed to velocity (delta compensates frame lag)
 	if(input_vector != Vector2.ZERO):
-		velocity = input_vector
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
 	else:
-		velocity = Vector2.ZERO
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-	move_and_collide(velocity)
+	# sets velocity to player
+	move_and_collide(velocity) 
