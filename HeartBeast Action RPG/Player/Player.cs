10d3ef0915a36ab15e2a4336a6cfc7250667cfad
Rodrigo Miguel Rojas s3784466 <s3784466@student.rmit.edu.au>
2020-10-20 23:55:44 +1100

    Completed episode 5

--- HeartBeast Action RPG/Player/Player.cs
+++ HeartBeast Action RPG/Player/Player.cs
@@ -10,13 +10,20 @@ public class Player : KinematicBody2D
     
     // variables
     private Vector2 velocity = Vector2.Zero;
+    private AnimationPlayer animationPlayer = null;
+    
+    // runs on load
+    public override void _Ready()
+    {
+        animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+    }
 
     // runs every frame
     public override void _PhysicsProcess(float delta) 
     {
         // player movement
         var inputVector = Vector2.Zero;
-        
+
         // checks for direction
         inputVector.x = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
         inputVector.y = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
@@ -25,10 +32,23 @@ public class Player : KinematicBody2D
         inputVector = inputVector.Normalized();
 
         // set velocity of player
-        if(inputVector != Vector2.Zero)
+        if (inputVector != Vector2.Zero)
+        {
+            // running animation
+            if(inputVector.x > 0)
+                animationPlayer.Play("RunRight");
+            else
+                animationPlayer.Play("RunLeft");
+            
             velocity = velocity.MoveToward(inputVector * MAX_SPEED, ACCELERATION * delta);
+        }
         else
+        {
+            // stop moving and run idle animation
+            animationPlayer.Play("IdleRight"); 
             velocity = velocity.MoveToward(Vector2.Zero, FRICTION * delta);
+        }
+            
 
         velocity = MoveAndSlide(velocity);
 
